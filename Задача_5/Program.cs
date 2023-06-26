abe8dba88028242822ba66a783cdc70de6417f34
unknown <dtroitskyi@gmail.comgit config --global user.name 'Denis>
2023-06-26 11:18:26 +0300

    26/06 2023

---
+++ Задача_5/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+int num = new Random().Next(10, 100);
+Console.WriteLine($"Максимnt num1 = num / 10;
+int num2 = num % 10;
+int max = num1;
+if (num2 > max)
+Int max = num2;альная цифра в числе {num} -> {max}")
