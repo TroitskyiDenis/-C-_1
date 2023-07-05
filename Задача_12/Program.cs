fa0878af8525ee7f931c0190a309b195733baff4
unknown <dtroitskyi@gmail.comgit config --global user.name 'Denis>
2023-07-05 10:40:17 +0300

    03/07/23

---
+++ Задача_12/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+//456 -> 3
+
+//78 -> 2
+
+//89126 -> 5
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+
+int Numbers(int number){
+    int count = 0;
+    if(number == 0){
+        count = 1;
+    }
+    else{
+        
+        while(number > 0){
+            number /= 10;
+            count++;
+        }
+    }
+
+    return count;
+}
+Console.WriteLine($"{num} -> {Numbers(num)}");
